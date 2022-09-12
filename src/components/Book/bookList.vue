f36d8ecbe6f8b09aa46759887e55f3c7341f6130
rezatati <rezatati@gmail.com>
2022-09-12 16:45:17 +0430

    add axios and init components

---
+++ src/components/Book/bookList.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>list of booksdd</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BookList></BookList>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import BookList from '@/components/Book/bookList.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+    BookList,
+  },
+};
 </script>
